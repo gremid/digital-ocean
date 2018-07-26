2eca70f4ccbf9fd5bfb9710f6004b12f1e70a259
Gregor Middell <gregor@middell.net>
2018-07-26 17:07:56 +0200

    Remove API-v1-related sources

--- test/digitalocean/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns digitalocean.core-test
-  (:use midje.sweet)
-  (:require [digitalocean.v2.core :as do]))
-
-(facts "about resource urls"
-  (do/resource-url :domains 1 2 3) => "https://api.digitalocean.com/v2/domains/1/2/3"
-  (do/resource-url :domains) => "https://api.digitalocean.com/v2/domains/")
